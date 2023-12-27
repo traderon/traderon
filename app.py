c8254917937d669cb89b689f212ff7c6c19f5a77
jinlao001 <kifo1881@outlook.com>
2023-12-28 06:55:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,6 @@ def get_trade_data():
                         "date": sub.date, "size": sub.size, "position": sub.position, "price": sub.price, })
         data_array.append({"id": data.trade_id, "broker": data.broker, "status": data.status, "openDate": data.open_date, "symbol": data.symbol,
                           "entry": data.entry, "exit": data.exit, "size": data.size, "return": data.ret, "side": data.side, "setups": data.setups, "mistakes": data.mistakes, "subs": subs})
-    #hola
     return jsonify(data_array)
 
 
