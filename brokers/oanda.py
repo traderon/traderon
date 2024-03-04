914fa266969e8ba195334b1baa2adacb0a78bf08
perfectcode915 <kifo1881@outlook.com>
2024-03-05 06:18:55 +0900

    Update oanda.py

--- brokers/oanda.py
+++ brokers/oanda.py
@@ -51,6 +51,10 @@ def oanda_import(api_key, account_ID):
                     action_2 = 'Buy'
                 pips = (float(trade['averageClosePrice']) -
                         float(trade['price'])) * float(trade['initialUnits']) * 100
+                return_percent = abs(float(
+                    trade["realizedPL"]) * 500 / float(trade["price"]) / (float(trade["realizedPL"]) / pips) / float(trade["initialUnits"]))
+                if (float(trade["realizedPL"]) < 0):
+                    return_percent = 0 - return_percent
                 for appended in return_value:
                     sub_F = appended["subs"][0]
                     sub_L = appended['subs'][-1]
@@ -75,6 +79,8 @@ def oanda_import(api_key, account_ID):
                         appended['subs'].sort(key=sort_by_date)
                         temp = 0
                         for fixing in appended['subs']:
+                            if temp == 0:
+                                openPrice = fixing['price']
                             if fixing['action'] == 'Buy':
                                 temp += float(fixing['size'])
                             else:
@@ -82,11 +88,16 @@ def oanda_import(api_key, account_ID):
                             fixing['position'] = str(format(temp, '.2f'))
                         appended['exit'] = appended['subs'][-1]['price']
                         found = True
+                        appended["ret_percent"] = str(format(abs(float(appended["ret"]) * 500 / float(openPrice) / (
+                            float(appended["ret"]) / float(appended['pips'])) / float(appended["size"])), '.5f'))
+                        if float(appended['ret']) < 0:
+                            appended['ret_percent'] = "-" + \
+                                appended['ret_percent']
                         break
                 if found == True:
                     continue
                 return_value.append(
-                    {"account_id": account_ID, "broker": "Oanda", "trade_id": trade["id"], "status": status, "open_date": trade["openTime"], "symbol": instrument, "entry": trade["price"], "exit": trade["averageClosePrice"], "size": trade["initialUnits"], "pips": str(format(pips, '.5f')), "ret_pips": str(format(float(trade["realizedPL"]) / abs(pips), '.10f')), "ret": trade["realizedPL"], "ret_percent": "0", "ret_net": trade["realizedPL"], "side": side, "setups": "", "mistakes": "", "subs": [{"action": action, "spread": "SINGLE", "type": "FOREX", "date": trade["openTime"], "size": str(abs(float(trade["initialUnits"]))), "position": trade["initialUnits"], "price": trade["price"]}, {"action": action_2, "spread": "SINGLE", "type": "FOREX", "date": trade["closeTime"], "size": str(abs(float(trade["initialUnits"]))), "position": "0", "price": trade["averageClosePrice"]}]})
+                    {"account_id": account_ID, "broker": "Oanda", "trade_id": trade["id"], "status": status, "open_date": trade["openTime"], "symbol": instrument, "entry": trade["price"], "exit": trade["averageClosePrice"], "size": trade["initialUnits"], "pips": str(format(pips, '.5f')), "ret_pips": str(format(float(trade["realizedPL"]) / abs(pips), '.10f')), "ret": trade["realizedPL"], "ret_percent": str(format(return_percent, '.5f')), "ret_net": trade["realizedPL"], "side": side, "setups": "", "mistakes": "", "subs": [{"action": action, "spread": "SINGLE", "type": "FOREX", "date": trade["openTime"], "size": str(abs(float(trade["initialUnits"]))), "position": trade["initialUnits"], "price": trade["price"]}, {"action": action_2, "spread": "SINGLE", "type": "FOREX", "date": trade["closeTime"], "size": str(abs(float(trade["initialUnits"]))), "position": "0", "price": trade["averageClosePrice"]}]})
         return {'trades': return_value}
     except Exception as e:
         print(e)
